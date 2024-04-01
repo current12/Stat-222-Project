3a0615e1153998f3545c024e174f7e4fcea1ed70
Isaac Liu <ijyliu@uchicago.edu>
2024-03-31 18:28:58 -0700

    stattotex update

--- Code/Exploratory Data Analysis/All Data/stattotex.py
+++ Code/Exploratory Data Analysis/All Data/stattotex.py
@@ -1,5 +1,14 @@
+import os
+
 # Function that takes number and associated name from Python and saves into a file that allows for easy-read-in and later formatting in LaTeX
-def stattotex(number, number_name, filename):
+def stattotex(number, number_name, filename, clear_preexisting=True):
+
+    # Clearing the file if specified
+    if clear_preexisting:
+        # Delete the file if it exists
+        if os.path.exists(filename):
+            os.remove(filename)
+
     # Creating the LaTeX command
     command = f"\\newcommand{{\\{number_name}}}{{{number}}}"
 
