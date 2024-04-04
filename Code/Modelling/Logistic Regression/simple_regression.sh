33ac43eda51cbda668732f99d4a89acacf0f9433
Isaac Liu <ijyliu@uchicago.edu>
2024-04-03 23:57:29 -0700

    add .sh script

---
+++ Code/Modelling/Logistic Regression/simple_regression.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=simple_regression
+#SBATCH --output=simple_regression.out
+#SBATCH --partition=jsteinhardt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+
+# Timing
+# Reset the SECONDS variable
+SECONDS=0
+
+echo "Starting Job"
+
+# Execute the notebook
+jupyter nbconvert --to notebook --execute --inplace simple_regression.ipynb
+
+echo "Completed Job"
+
+# Calculate time in minutes
+elapsed_minutes=$((SECONDS / 60))
+# Print the time elapsed in minutes
+echo "Time elapsed: $elapsed_minutes minute(s)."
