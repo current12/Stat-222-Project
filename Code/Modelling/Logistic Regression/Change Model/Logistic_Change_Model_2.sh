aa6a95638b472734cfe2c5cead0907d454545af2
Owen <owenlin2002@gmail.com>
2024-05-01 21:28:17 -0700

    shell script for scf

---
+++ Code/Modelling/Logistic Regression/Change Model/Logistic_Change_Model_2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=Logistic_Change_Model_2
+#SBATCH --output=Logistic_Change_Model_2.out
+#SBATCH --partition=lowmem
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+# Timing
+# Reset the SECONDS variable
+SECONDS=0
+
+echo "Starting Job"
+
+# Execute the notebook
+jupyter nbconvert --to notebook --execute --inplace Logistic_Change_Model_2.ipynb
+
+echo "Completed Job"
+
+# Calculate time in minutes
+elapsed_minutes=$((SECONDS / 60))
+# Print the time elapsed in minutes
+echo "Time elapsed: $elapsed_minutes minute(s)."
